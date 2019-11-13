017723caeeb3c5803f00cf783e8fb849dbf8c845
Arzu1996 <harzu2996@gmail.com>
2019-11-13 12:19:29 +0400

    --create project

---
+++ src/main/java/step/Menu.java
@@ -0,0 +1,18 @@
+package step;
+
+public class Menu {
+
+  public String show() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("========================").append('\n');
+    sb.append("|    Booking App       |").append('\n');
+    sb.append("========================").append('\n');
+    sb.append("| 1. Show timetable    |").append('\n');
+    sb.append("| 2. Make a booking    |").append('\n');
+    sb.append("| 3. Show my bookings  |").append('\n');
+    sb.append("| 3. Cancel my booking |").append('\n');
+    sb.append("| 4. Exit              |").append('\n');
+    sb.append("========================").append('\n');
+    return sb.toString();
+  }
+}
